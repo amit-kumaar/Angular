d35ec194c4f2e6c080177eaecc53ea4edba9d5bb
amit-kumaar <amitk.umar@yahoo.com>
2026-02-18 16:57:16 +0530

    assignment-1

---
+++ app.js
@@ -0,0 +1,28 @@
+(function(){
+    'use strict';
+
+    angular.module('LunchCheck',[])
+     .controller('LunchCheckController',LunchCheckController);
+
+     LunchCheckController.$inject=['$scope'];
+     function LunchCheckController($scope){
+        $scope.items="";
+        $scope.message="";
+
+        $scope.checkTooMuch=function(){
+            if(!$scope.items){
+                $scope.message="Please enter data first";
+                return;
+            }
+            var itemArray=$scope.items.split(',');
+
+            if(itemArray.length<=3){
+                $scope.message="Enjoy!";
+            }
+            else{
+                $scope.message="Too much!";
+            }
+
+            };
+        }
+})();
